8114c74540c8ed8004ce0c6e76f8b24254ec8d8f
AtharvTungatkar <atharvpt52@gmail.com>
2025-08-13 21:52:24 +0530

    Deployment ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Dog-Breed-Classification",
+    version="0.0.1",
+    author="Atharv Prashant Tungatkar",
+    author_email="atharvpt52@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
